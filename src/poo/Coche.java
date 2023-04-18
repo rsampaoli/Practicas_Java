01d327d94fb0388b882ea3863135ae8a70bcd30c
ramirosampaoli <ramirosampaoli@gmail.com>
2023-04-18 15:59:02 -0300

    Operando JAVA POO

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -52,5 +52,48 @@ public class Coche {
         } else {
             return "EL coche tiene asientos de serie";
         }
+    };
+
+    public void configura_climatizador(String climatizador) { // SETTER
+        if (climatizador == "si") {
+            this.climatizador = true;
+        } else {
+            this.climatizador = false;
+        }
+    };
+
+    public String dime_climatizador() { // GETTER
+        if (climatizador = true) {
+            return "El coche tiene climatizador";
+        } else {
+            return "El coche no tiene climatizador";
+        }
+    };
+
+    public String dime_peso_coche() { // SETTER + GETTER
+        int peso_carroceria = 500;
+
+        peso_final = peso + peso_carroceria;
+
+        if (asientos_cuero == true) {
+            peso_final = peso_final + 50;
+        }
+
+        if (climatizador == true) {
+            peso_final = peso_final + 20;
+        }
+
+        return "El peso del coche es " + peso_final + " KG";
+    }
+
+    public int precio_coche() { // GETTER
+        int precio_final = 10000;
+        if (asientos_cuero == true) {
+            precio_final += 2000;
+        }
+        if (climatizador == true) {
+            precio_final += 1500;
+        }
+        return precio_final;
     }
 }
